55be889a14c4fea958f1712ed3e0ef4fe3ab7515
sammpath <sammpath@gmail.com>
2024-05-13 10:30:51 -0500

    Update config.js

--- amazon-location-route-planner/src/config.js
+++ amazon-location-route-planner/src/config.js
@@ -1,5 +1,5 @@
-const identityPoolId = "IDENTITY_POOL_ID";
-const mapName = "TruckDemoMap";
+const identityPoolId = "us-west-2:2402ae9f-6bb1-43e3-b949-1dbeddbdeca1";
+const mapName = "exampleMap";
 const routeCalculatorName = "TruckDemoRouteCalculator";
 
 export { identityPoolId, mapName, routeCalculatorName };
